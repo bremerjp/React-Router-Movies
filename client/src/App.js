a4a79b01964c7cc6f8d6cf200347a8101372987e
Jacob Bremer <bremerjp@gmail.com>
2019-12-16 16:48:28 -0500

    completed part 2, added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -12,12 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path="/">
-        <MovieList />
-      </Route>
-      <Route path="/movies/:itemID">
-        <Movie />
-      </Route>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };
